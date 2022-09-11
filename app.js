fc61ce7f15f02842c1763118e191f079093aadc2
123 <ppotter025@gmail.com>
2022-09-11 13:58:14 +0800

    feat: add passport

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
+const session = require('express-session')
+const usePassport = require('./config/passport')
 const PORT = 3000
+const passport = require('passport')
 
 const db = require('./models')
 const Todo = db.Todo 
@@ -11,8 +14,16 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+  }))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
     return Todo.findAll({
@@ -34,9 +45,11 @@ app.get('/users/login', (req, res) => {
     res.render('login')
 })
   
-app.post('/users/login', (req, res) => {
-    res.send('login')
-})
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  }))
   
 app.get('/users/register', (req, res) => {
     res.render('register')
